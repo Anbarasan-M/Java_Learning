56a93b9026273653804c53d1e27284483e78a59a
Anbarasan <anbumuthu0509@gmail.com>
2023-07-16 14:31:22 +0530

    primitive_data_types

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(args[2]);
-        System.out.println("Hello world!");
-    }
-    public static void main1(String[] args) {
-        System.out.println("Hello world!");
-    }
+class primitive_data_types{
+    public static void main(String[] args){
+        int num1 = 1;
+        byte num2 = 127;
+        short num3 = 32766;
+        long num4 = 234576423690L;
 
+        float num5 = 2.3f;
+        double num6 = 2.3342;
+
+        char c = 'A';
+
+        boolean b = true;
+    }
 }
