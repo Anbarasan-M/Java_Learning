80b2313d582551060b15ad5547d9cc80311cf63b
Anbarasan <anbumuthu0509@gmail.com>
2023-07-17 13:08:54 +0530

    primitive_data_types

---
+++ Daily_Practice_Codes/Main.java
@@ -0,0 +1,15 @@
+class primitive_datatype{
+    public static void main(String[] args){
+        int i = 1;            // no exceptions
+        byte b = 123;         // must betweeen -127 to +127
+        short s = 12345;
+        long l = 1234567890;
+
+        float f = 12.45f;
+        double d = 12.32;
+
+        char c = 'K';
+
+        boolean bool = true;
+    }
+}
