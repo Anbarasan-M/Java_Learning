6f69618f7f529bf93c16a5c000272aa74b7d8ec3
Anbarasan <anbumuthu0509@gmail.com>
2023-07-30 21:41:31 +0530

    Learning

--- src/com/practice/objectorientedprogramming/Constructor.java
+++ src/com/practice/objectorientedprogramming/Constructor.java
@@ -1,4 +1,29 @@
 package com.practice.objectorientedprogramming;
 
-public class Constructor {
+class Demo
+{
+    String name;
+    int age;
+
+    public Demo()
+    {
+        System.out.print("Default Constructor");
+    }
+
+//    Constructor name always should be like class name
+    public Demo(int a, String n) // Parameterised constructor
+    {
+        name = n;
+        age = a;
+        System.out.println("Constructor called");
+    }
+}
+public class Constructor
+{
+    public static void main(String []args)
+    {
+//        A constructor is always called everytime an object is created - it is a default operation
+        Demo obj = new Demo(21, "Anbu");
+        System.out.println(obj.age + " " + obj.name);
+    }
 }
