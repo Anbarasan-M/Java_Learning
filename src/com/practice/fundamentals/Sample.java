dc561542fe45875099c9a4fc6ad2fede9342254e
Anbarasan <anbumuthu0509@gmail.com>
2023-07-31 12:47:15 +0530

    Learning

---
+++ src/com/practice/fundamentals/Sample.java
@@ -0,0 +1,13 @@
+package com.practice.fundamentals;
+
+class Demo{
+    public Demo(){
+        System.out.println("It's Working");
+    }
+}
+
+public class Sample {
+    public static void main(String[] args){
+        new Demo();
+    }
+}
---
+++ src/com/practice/objectorientedprogramming/Inheritance.java
@@ -0,0 +1,8 @@
+package com.practice.objectorientedprogramming;
+
+public class Inheritance {
+    public static void main(String []args)
+    {
+
+    }
+}
