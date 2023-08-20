8add27cc2c6413e4c3824568844772af8d007ea7
Anbarasan <anbumuthu0509@gmail.com>
2023-08-20 12:59:12 +0530

    Learning

--- src/com/practice/collection/ArrayListCollection.java
+++ src/com/practice/collection/ArrayListCollection.java
@@ -1,6 +1,7 @@
 package com.practice.collection;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ArrayListCollection
@@ -9,19 +10,20 @@ public class ArrayListCollection
     {
         // ArrayList
         //Collection<Integer> array = new ArrayList<>();  Arraylist is a class which implements a list
-        List<Integer> array = new ArrayList<>();
+        List<Object> array = new ArrayList<>();
 
         // If you just want to add and use values, collection is suitable
         // If you want to perform operations using index values list is suitable
 
         array.add(2);
         array.add(4);
-        array.add(6);
+        array.add(10);
         array.add(8);
+        array.add("Anbu");
 
         System.out.println(array.get(0));
         System.out.println(array.indexOf(6));
-        System.out.println(array);
+        System.out.println(Arrays.toString(array.toArray()));
 
     }
 }
