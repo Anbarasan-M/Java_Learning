29f879e73d36bc5072f2220f414d17c2afaf4d7f
Anbarasan <anbumuthu0509@gmail.com>
2023-08-23 22:53:58 +0530

    Learning

--- src/com/practice/fundamentals/Sample.java
+++ src/com/practice/fundamentals/Sample.java
@@ -315,9 +315,93 @@ import java.util.List;
 //    }
 //}
 
+class Node
+{
+    Node next;
+    int data;
+}
+class LinkingList
+{
+    Node head;
 
+    public void insert(int data)
+    {
+        Node node = new Node();
+        node.data = data;
+        node.next = null;
+
+        if (head == null)
+        {
+            head = node;
+        }
+        else
+        {
+            Node n = head;
+            while(n.next != null)
+            {
+                n = n.next;
+            }
+            n.next = node;
+        }
+    }
+    public void insertAtStart(int data)
+    {
+        Node node = new Node();
+
+        node.data = data;
+        node.next = head;
+
+        head = node;
+    }
+    public void insertAt(int index, int data)
+    {
+        Node node = new Node();
+
+        node.data = data;
+        node.next = null;
+
+        if (index == 0)
+        {
+            insertAtStart(data);
+        }
+        else
+        {
+            Node n = head;
+
+            for (int i = 0; i < index-1; i ++)
+            {
+                n = n.next;
+            }
+            node.next = n.next;
+            n.next = node;
+        }
+    }
+    public void delete(int index, int data)
+    {
+        Node n = head;
+
+    }
+    public void show()
+    {
+        Node node = head;
+        while (node.next != null)
+        {
+            System.out.println(node.data);
+            node = node.next;
+        }
+        System.out.println(node.data);
+    }
+}
 public class Sample {
     public static void main(String[] args) {
+        LinkingList list = new LinkingList();
+        list.insert(45);
+        list.insert(32);
+        list.insert(15);
+        list.insert(90);
+        list.insertAt(0, 100);
+        list.insertAtStart(56);
 
+        list.show();
     }
 }
---
+++ src/com/practice/problems/MiddleOfTheLikedList.java
@@ -0,0 +1,52 @@
+package com.practice.problems;
+
+import java.util.AbstractList;
+
+class Node {
+    int data;
+    Node next;
+}
+class LinkList1
+{
+    Node head;
+
+    public void insert(int data)
+    {
+        Node node = new Node();
+
+        node.data = data;
+        node.next = null;
+
+        if (head == null)
+        {
+            head = node;
+        }
+        else
+        {
+            Node n = head;
+            while(n.next != null)
+            {
+                n = n.next;
+            }
+            n.next = node;
+        }
+    }
+}
+
+public class MiddleOfTheLikedList
+{
+    public static void main(String[] args)
+    {
+        LinkList1 list = new LinkList1();
+
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+        list.insert(5);
+
+        Node node = new Node();
+
+
+    }
+}
