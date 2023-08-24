874d71eefe2b8da8d25acb918114ab72ad2ba29c
Anbarasan <anbumuthu0509@gmail.com>
2023-08-24 16:06:51 +0530

    Learning

---
+++ src/com/practice/collection/HashSet.java
@@ -0,0 +1,24 @@
+package com.practice.collection;
+import java.util.Iterator;
+import java.util.Set;
+
+public class HashSet
+{
+    public static void main(String [] args)
+    {
+        Set<String> names = new java.util.HashSet<>();
+
+        names.add("Kowsalya");
+        names.add("Gowri");
+        names.add("Kalayarasan");
+        names.add("Anbarasan");
+        names.add("Anbarasan");  // Cannot take duplicates
+
+        Iterator<String> nIterator = names.iterator();
+
+        while(nIterator.hasNext())
+        {
+            System.out.println(nIterator.next());
+        }
+    }
+}
