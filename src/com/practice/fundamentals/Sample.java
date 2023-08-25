b55adef82d4bdd001409b14f881e1c522b643b2d
Anbarasan <anbumuthu0509@gmail.com>
2023-08-25 21:06:25 +0530

    Learning

--- src/com/practice/fundamentals/Sample.java
+++ src/com/practice/fundamentals/Sample.java
@@ -1,9 +1,21 @@
 package com.practice.fundamentals;
-
+class ASAESolution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String str1 = "";
+        String str2 = "";
+        for(String s1:word1) {
+            str1 = str1.concat(s1);
+        }
+        for(String s2:word2) {
+            str2 = str2.concat(s2);
+        }
+        return str1.equals(str2);
+    }
+}
 public class Sample {
     public static void main(String[] args) {
-        int a = 1234;
-        String str = String.valueOf(a);
-
+        ASAESolution obj = new ASAESolution();
+        boolean result = obj.arrayStringsAreEqual(new String[]{"ab", "c"},new String[]{"a", "bc"});
+        System.out.println(result);
     }
 }
