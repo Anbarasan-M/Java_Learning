2e72e38e2e3ab3ef0aac66131a4684fb823a6aa8
Anbarasan <anbumuthu0509@gmail.com>
2023-08-27 22:58:58 +0530

    Learning

---
+++ src/com/practice/problems/AcronymOfWords.java
@@ -0,0 +1,26 @@
+package com.practice.problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class AOWSolution {
+    public boolean isAcronym(List<String> words, String s) {
+        String ans = "";
+        for (String i:words){
+            char j = i.charAt(0);
+            ans += j;
+        }
+        return ans.equals(s);
+    }
+}
+public class AcronymOfWords {
+    public static void main(String [] args){
+        List<String> words = new ArrayList<>();
+        words.add("allice");
+        words.add("bob");
+        words.add("charlie");
+        AOWSolution obj = new AOWSolution();
+        boolean result = obj.isAcronym(words, "abc");
+        System.out.println(result);
+    }
+}
