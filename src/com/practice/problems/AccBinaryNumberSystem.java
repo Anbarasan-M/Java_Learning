49fac35b6f6de67e5bfccad46c03522290626c0f
Anbarasan <anbumuthu0509@gmail.com>
2023-09-08 16:53:53 +0530

    Learning

--- src/com/practice/problems/AccBinaryNumberSystem.java
+++ src/com/practice/problems/AccBinaryNumberSystem.java
@@ -1,21 +1,32 @@
-//package com.practice.problems;
-//
-//public class AccBinaryNumberSystem {
-//    public static int binaryOperation(String s, int n){
-//        if(n == 0){
-//           return -1;
-//        }
-//        char a = s.charAt(0);
-//        int i = 1;
-//        while(i<n){
-//            if(s.charAt(i) == 'A'){
-//                a&=s.charAt(i+1);
-//            }if(s.charAt(i) == 'B'){
-//                a|=s.charAt(i+1);
-//            }if(s.charAt(i) == 'C'){
-//                a^=s.charAt(i+1);
-//            }
-//        }
-//        i += 2;
-//    }
-//}
+package com.practice.problems;
+
+import java.util.Scanner;
+
+public class AccBinaryNumberSystem {
+    public static int binaryOperation(String s){
+        int res = s.charAt(0) - '0';
+        for(int i = 1; i < s.length();){
+            char letter = s.charAt(i);
+            i++;
+            if(letter == 'A'){
+                res &= s.charAt(i) - '0';
+            }
+            else if(letter == 'B'){
+                res |= s.charAt(i) - '0';
+            }else if(letter == 'C'){
+                res ^= s.charAt(i) - '0';
+            }
+            i++;
+        }
+        return res;
+    }
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the sequence");
+        String s = sc.next();
+
+        System.out.println(binaryOperation(s));
+    }
+}
