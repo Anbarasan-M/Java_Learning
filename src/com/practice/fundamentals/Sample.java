ce49bd0d68f080af94e41350bcd0d57b20e11dcb
Anbarasan <anbumuthu0509@gmail.com>
2023-09-12 22:28:21 +0530

    Learning

--- src/com/practice/fundamentals/Sample.java
+++ src/com/practice/fundamentals/Sample.java
@@ -4,8 +4,16 @@ import java.lang.*;
 class Main
 {
     public static void main(String[] args)
-    {
 
+    {
+        int a = 65;
+        char b = (char) a;
+        char c = 'k';
+        char d = 'L';
+        StringBuilder sb = new StringBuilder();
+        String m = "lhdsag";
+        String n = "ldh";
+        System.out.println(m+n);
     }
 
 }
---
+++ src/com/practice/problems/AccNBaseNotation.java
@@ -0,0 +1,21 @@
+package com.practice.problems;
+
+public class AccNBaseNotation {
+    public static void main(String[] args){
+
+        String string = "Move-Hyphens-to-Front";
+        String ans = "";
+
+        for(int i=0; i<string.length(); i++){
+            if(string.charAt(i) == '-'){
+                ans = string.charAt(i) + ans;
+                i++;
+            }
+            else;
+            {
+                ans += string.charAt(i);
+            }
+        }
+        System.out.println(ans);
+    }
+}
