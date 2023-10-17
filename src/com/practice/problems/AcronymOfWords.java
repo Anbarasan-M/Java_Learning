4a4a77ef4eda9d951073c7a187acfedf03074f9b
Anbarasan <anbumuthu0509@gmail.com>
2023-10-17 10:23:00 +0530

    Learning

--- src/com/practice/problems/AcronymOfWords.java
+++ src/com/practice/problems/AcronymOfWords.java
@@ -1,26 +1,22 @@
 package com.practice.problems;
-
 import java.util.ArrayList;
 import java.util.List;
 
-class AOWSolution {
-    public boolean isAcronym(List<String> words, String s) {
-        StringBuilder ans = new StringBuilder();
-        for (String i:words){
-            char j = i.charAt(0);
-            ans.append(j);
-        }
-        return ans.toString().equals(s);
-    }
-}
 public class AcronymOfWords {
     public static void main(String [] args){
         List<String> words = new ArrayList<>();
         words.add("allice");
         words.add("bob");
         words.add("charlie");
-        AOWSolution obj = new AOWSolution();
-        boolean result = obj.isAcronym(words, "abc");
+        boolean result = AcronymOfWords.isAcronym(words, "abc");
         System.out.println(result);
+    }
+    public static boolean isAcronym(List<String> words, String s) {
+        StringBuilder ans = new StringBuilder();
+        for (String i:words){
+            char j = i.charAt(0);
+            ans.append(j);
+        }
+        return ans.toString().equals(s);
     }
 }
