1950497c224a0d39c014d386fcd6d68bf47a2553
Anbarasan <anbumuthu0509@gmail.com>
2023-10-17 10:22:13 +0530

    Learning

--- src/com/practice/problems/ArrayStringsAreEquivalent.java
+++ src/com/practice/problems/ArrayStringsAreEquivalent.java
@@ -1,7 +1,11 @@
 package com.practice.problems;
 
-class ASAESolution {
-    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+public class ArrayStringsAreEquivalent {
+    public static void main(String[] args) {
+        boolean result = ArrayStringsAreEquivalent.arrayStringsAreEqual(new String[]{"ab", "c"},new String[]{"a", "bc"});
+        System.out.println(result);
+    }
+    public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
         String str1 = "";
         String str2 = "";
         for(String s1:word1) {
@@ -11,13 +15,5 @@ class ASAESolution {
             str2 = str2.concat(s2);
         }
         return str1.equals(str2);
-    }
-}
-
-public class ArrayStringsAreEquivalent {
-    public static void main(String[] args) {
-        ASAESolution obj = new ASAESolution();
-        boolean result = obj.arrayStringsAreEqual(new String[]{"ab", "c"},new String[]{"a", "bc"});
-        System.out.println(result);
     }
 }
